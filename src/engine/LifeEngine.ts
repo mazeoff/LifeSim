e89139d246448e6b31cab7215b6578a3a49b0ad9
Gleb Nikonorov <maze0ff@ya.ru>
2026-09-09 12:34:54 +0300

    add history of states; refactoring

--- src/engine/LifeEngine.ts
+++ src/engine/LifeEngine.ts
@@ -2,12 +2,20 @@ export class LifeEngine {
     readonly width: number;
     readonly height: number;
 
-    private grid: Uint8Array;
+    private history: Uint8Array[] = [];
+    private currentStep: number = 0;
 
     constructor (width: number, height: number) {
         this.width = width;
         this.height = height;
-        this.grid = new Uint8Array(width * height);
+
+        this.history.push(
+            new Uint8Array(new Uint8Array(width * height))
+        );
+    }
+
+    private get currentGrid(): Uint8Array {
+        return this.history[this.currentStep];
     }
 
     private getIndex(x: number, y: number): number {
@@ -19,25 +27,39 @@ export class LifeEngine {
     }
 
     public get cells(): Uint8Array {
-        return this.grid;
+        return new Uint8Array(this.currentGrid);
+    }
+
+    public get stepNumber(): number {
+        return this.currentStep;
+    }
+
+    public get historyLength(): number {
+        return this.history.length;
+    }
+
+    public getHistoryStep(step: number): Uint8Array | undefined {
+        const state = this.history[step];
+
+        return !step ? undefined : new Uint8Array(state)
     }
     
     public setCell(x: number, y: number, isAlive: boolean) {
-        this.grid[this.getIndex(x, y)] = Number(isAlive);
+        this.currentGrid[this.getIndex(x, y)] = Number(isAlive);
     }
 
     public toggleCell(x: number, y: number) {
         const index = this.getIndex(x, y);
 
-        this.grid[index] = Number(!this.grid[index]);
+        this.currentGrid[index] = Number(!this.currentGrid[index]);
     }
 
     public isAlive(x: number, y: number): boolean {
-        return Boolean(this.grid[this.getIndex(x, y)]);
+        return Boolean(this.currentGrid[this.getIndex(x, y)]);
     }
 
     public clear() {
-        this.grid.fill(0);
+        this.currentGrid.fill(0);
     }
 
     public countNeighbors(x: number, y: number): number {
@@ -54,7 +76,6 @@ export class LifeEngine {
                     y: this.wrap(y + dy, this.height)
                 };
 
-
                 if (this.isAlive(neighborCell.x, neighborCell.y)) {
                     count++;
                 }
@@ -65,22 +86,23 @@ export class LifeEngine {
     }
 
     public step() {
-        const nextGrid = new Uint8Array(this.grid.length);
+        const currentGrid = this.currentGrid;
+        const nextGrid = new Uint8Array(currentGrid.length);
 
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
                 const index = this.getIndex(x, y);
-                const isAlive = this.isAlive(x, y);
+
+                const isAlive = Boolean(currentGrid[index]);
                 const neighborsCount = this.countNeighbors(x, y);
 
-                if (isAlive) {
-                    nextGrid[index] = Number(neighborsCount === 2 || neighborsCount === 3);
-                } else {
-                    nextGrid[index] = Number(neighborsCount === 3);
-                }
+                nextGrid[index] = isAlive
+                    ? Number(neighborsCount === 2 || neighborsCount === 3)
+                    : Number(neighborsCount === 3);
             }
         }
 
-        this.grid = nextGrid;
+        this.history.push(nextGrid);
+        this.currentStep++;
     }
 }
