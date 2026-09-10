4b6a9d39bb3424647ca1b7b6b10483c8b2f0e5fa
Gleb Nikonorov <maze0ff@ya.ru>
2026-09-10 10:57:05 +0300

    refactoring app component: add game board, add app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import GameBoard from "./components/GameBoard/GameBoard"
+import { LifeEngine } from "./engine/LifeEngine";
+import styles from './App.module.scss';
+import { DEFAULT_AREA_SETTINGS } from "./engine/constants";
+
+const { width, height } = DEFAULT_AREA_SETTINGS;
+const engine = new LifeEngine(width, height);
+
 function App() {
 
-  return (
-    <></>
-  )
+    return (
+        <main className={styles.app}>
+            <GameBoard engine={engine} />
+        </main>
+    )
 }
 
-export default App
+export default App;
