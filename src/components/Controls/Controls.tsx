b3078e61d9369101f226f61d5282c855db03bba6
Gleb Nikonorov <maze0ff@ya.ru>
2026-09-10 11:13:41 +0300

    add controls component

---
+++ src/components/Controls/Controls.tsx
@@ -0,0 +1,53 @@
+import { observer } from "mobx-react-lite";
+import { LANGS } from "../../engine/constants";
+import type { LifeEngine } from "../../engine/LifeEngine";
+import styles from './Controls.module.scss';
+
+
+type ControlsProps = {
+    engine: LifeEngine;
+};
+
+function Controls(props: ControlsProps) {
+    const { engine } = props;
+
+    return (
+        <div className={styles.controls}>
+            <button
+                type="button"
+                disabled={!engine.canGoBack}
+                onClick={() => engine.prevStep()}
+            >
+                {LANGS.BACK}
+            </button>
+
+            <span>
+                {LANGS.STEP}: {engine.stepNumber}
+            </span>
+
+            <button
+                type="button"
+                disabled={engine.isFinished && !engine.canGoForward}
+                onClick={() => engine.nextStep()}
+            >
+                {LANGS.FORWARD}
+            </button>
+
+            <button
+                type="button"
+                disabled={!engine.canEdit}
+                onClick={() => engine.clear()}
+            >
+                {LANGS.CLEAR}
+            </button>
+
+            {engine.isFinished && (
+                <span className={styles.finished}>
+                    {LANGS.FINISH}
+                </span>
+            )}
+        </div>
+    );
+}
+
+export default observer(Controls);
--- src/engine/constants.ts
+++ src/engine/constants.ts
@@ -2,3 +2,11 @@ export const DEFAULT_AREA_SETTINGS = {
     width: 50,
     height: 50,
 }
+
+export const LANGS = {
+    BACK: "Назад",
+    FORWARD: "Вперёд",
+    STEP: "Шаг",
+    CLEAR: "Очистить",
+    FINISH: "Симуляция завершена",
+}
